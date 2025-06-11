cd447bea7c281e819db7214c46c0486f33e21cc4
CJSadowitz <sadowitzc@gmail.com>
2025-06-11 12:30:28 -0400

    Support for dynamic track splitting, removed legacy code

--- node.py
+++ node.py
@@ -5,7 +5,7 @@ import math
 # Definitions
 MAX_ACCELERATION =  0.2 # m/s^2
 MIN_ACCELERATION = -0.2 # m/s^2
-MAX_VELOCITY = 7.8 # m/s 9.1
+MAX_VELOCITY = 10.6 # m/s 9.1
 BATTERY_CAPACITY = 5.1  # kW * hrs
 MAX_PANEL_POWER  = 0.976  # kW
 COEFFICIENT_DRAG = 0.22
@@ -24,25 +24,23 @@ class Node_Tree:
 		self.time_of_day = time_of_day
 		self.location = location
 
-		head = self.generate_tree()
+		head = self.generate_tree(100)
 		self.print_nodes(head)
 		self.print_track_stats(head)
 
-	def generate_tree(self):
-		total_position = 0
-		head = Node(self.time_of_day, MAX_ACCELERATION, 0, 0, self.start_percent, self.location)
-		total_position += head.end_position - head.start_position
+	def generate_tree(self, sections):
+		head = Node(sections, self.time_of_day, MAX_ACCELERATION, 0, 0, self.start_percent, self.location)
 		prev_node = head
-		while total_position < TRACK_LENGTH:
+		for i in range(sections - 1):
 			acceleration = MAX_ACCELERATION * 0.2
 			cur = Node(
+				sections,
 				self.time_of_day + datetime.timedelta(seconds=prev_node.section_time),
 				acceleration,
 				prev_node.end_velocity,
 				prev_node.end_position,
 				prev_node.end_percentage,
 				self.location)
-			total_position += cur.end_position - cur.start_position
 			prev_node.next_node = cur
 			prev_node = cur
 		return head
@@ -50,12 +48,12 @@ class Node_Tree:
 	def print_nodes(self, head):
 		while head != None:
 			print(f"""
-				Section_Time:      {head.section_time / 60:.2f} minutes
-				Battery:           {head.start_percentage * BATTERY_CAPACITY:.2f}, {head.end_percentage * BATTERY_CAPACITY:.2f}
-				Power_Used:        {1 - head.end_percentage / head.start_percentage:.2%}
-				End_Velocity:      {head.end_velocity:.2f} m/s
-				Average_Velocity:  {head.average_velocity:.2f} m/s
-				Distance_Traveled: {head.end_position - head.start_position:.2f} m
+Section_Time:      {head.section_time / 60:.2f} minutes
+Battery:           {head.start_percentage * BATTERY_CAPACITY:.2f}, {head.end_percentage * BATTERY_CAPACITY:.2f}
+Power_Used:        {1 - head.end_percentage / head.start_percentage:.2%}
+End_Velocity:      {head.end_velocity:.2f} m/s
+Average_Velocity:  {head.average_velocity:.2f} m/s
+Distance_Traveled: {head.end_position - head.start_position:.2f} m
 			""")
 			head = head.next_node
 
@@ -69,26 +67,27 @@ class Node_Tree:
 			distance   += head.end_position - head.start_position
 			head = head.next_node
 		print (f"Total_Track_Time: {track_time:.2f} minutes")
-		print (f"Laps_Time: {math.floor((self.duration / 60) / track_time):.2f}")
+		print (f"Laps_Time:        {math.floor((self.duration / 60) / track_time):.2f}")
 		print (f"Total_Power_Used: {power_used:.2%}")
-		print (f"Laps_Power: {math.floor((self.start_percent - self.end_percent) / power_used):.2f}")
+		print (f"Laps_Power:       {math.floor((self.start_percent - self.end_percent) / power_used):.2f}")
 		print (f"Total_Distance:   {distance:.2f} m")
 
 # Every node is responsable for 5% of the track distance.
 class Node:
-	def __init__(self, time, acceleration, start_v, start_d, start_p, location):
-		self.acceleration     = acceleration # m/s^2
-		self.start_velocity   = start_v      # m/s
-		self.end_velocity     = 0            # m/s
-		self.average_velocity = 0            # m/s
-		self.start_position   = start_d      # m
-		self.end_position     = start_d      # m
-		self.section_time     = 0            # s
-		self.start_percentage = start_p
-		self.end_percentage   = None
-		self.time = time # DATE
-		self.location = location # OBJECT
-		self.calc(start_p)
+	def __init__(self, sections, t, a, vi, pi, bi, location):
+		self.track_section    = sections
+		self.acceleration     = a  # m/s^2
+		self.start_velocity   = vi # m/s
+		self.end_velocity     = 0  # m/s
+		self.average_velocity = 0  # m/s
+		self.start_position   = pi # m
+		self.end_position     = pi # m
+		self.section_time     = 0  # s
+		self.start_percentage = bi
+		self.end_percentage   = 0
+		self.time = t             # DATE
+		self.location = location  # OBJECT
+		self.calc(bi)
 		self.next_node = None
 
 	def calc(self, start_p):
@@ -99,7 +98,7 @@ class Node:
 		vi = self.start_velocity
 		a = self.acceleration
 		vf = vi
-		for i in range(math.ceil(TRACK_LENGTH * 0.05)):
+		for i in range(math.ceil(TRACK_LENGTH / self.track_section)):
 			if (vf >= MAX_VELOCITY):
 				a = 0
 
@@ -112,7 +111,7 @@ class Node:
 		
 		self.section_time = sum(times)
 
-		self.end_position = TRACK_LENGTH * 0.05 + self.start_position
+		self.end_position = TRACK_LENGTH / self.track_section + self.start_position
 
 		if len(velocities) != 0:
 			self.average_velocity = sum(velocities) / len(velocities)
@@ -158,6 +157,8 @@ class Node:
 			# W * s
 			power += forces * v[i] * t[i]
 		# GRAVITY
+
+		# PARASITIC LOSSES
 
 		# W * hrs
 		power = power / 3600
