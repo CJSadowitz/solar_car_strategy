61b22773cb9559db174f06e7398d1aff2760f8ca
CJSadowitz <sadowitzc@gmail.com>
2025-06-16 14:42:11 -0400

    Adjusted constants, easier to use

--- constants.py
+++ constants.py
@@ -1,16 +1,24 @@
-# Definitions
-MAX_ACCELERATION               = 0.2            # m/s^2
-MIN_ACCELERATION               = -0.2           # m/s^2
+# Car Values
 MAX_VELOCITY                   = 15.0            # m/s 9.1
 BATTERY_CAPACITY               = 5.24           # kW * hrs
 MAX_PANEL_POWER                = 0.956          # kW
 MASS                           = 430            # kg
 GRAVITY                        = 9.81           # m/s^2
-WEIGHT                         = MASS * GRAVITY # N
 FRONTAL_AREA                   = 1.2            # m^2
-TRACK_LENGTH                   = 5069.434       # m
-PARASITIC_FACTOR               = 1.0            # %
+MOTOR_EFFICIENCY               = 0.80
+WEIGHT                         = MASS * GRAVITY # N
 COEFFICIENT_DRAG               = 0.3
 COEFFICIENT_ROLLING_RESISTANCE = 0.0055
-SECTIONS                       = 10
-MOTOR_EFFICIENCY               = 0.80
+
+# Fudge
+MAX_ACCELERATION               = 0.2            # m/s^2
+MIN_ACCELERATION               = -0.2           # m/s^2
+PARASITIC_FACTOR               = 1.0            # %
+
+# Track Data
+TRACK_LENGTH                   = 5069.434       # m
+
+# Code adjustments
+BATTERY_TIME_TOLERANCE         = 0.01
+SECTIONS                       = 100
+
